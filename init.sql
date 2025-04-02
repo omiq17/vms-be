c9b44106568b087f7ed3d4bd295fc590326a055e
omiq17 <omiq17@gmail.com>
2025-04-03 00:05:38 +0800

    Update sql.

--- init.sql
+++ init.sql
@@ -17,7 +17,7 @@ END$$;
 CREATE TABLE IF NOT EXISTS vms.vehicle (
     id SERIAL PRIMARY KEY,
     type vms.vehicle_type NOT NULL,  -- Using ENUM for type
-    is_locked BOOLEAN DEFAULT false,
+    is_locked BOOLEAN DEFAULT TRUE,
     speed INTEGER NOT NULL DEFAULT 0,
     battery INTEGER NOT NULL,
     status vms.vehicle_status NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS vms.vehicle (
 INSERT INTO vms.vehicle (type, is_locked, speed, battery, status, latitude, longitude, updated_on)
 VALUES
     ('SCOOTER', true, 0, 100, 'PARKING', 3.142012, 101.123456, '2019-07-02 09:00:00'),
-    ('SCOOTER', false, 5, 75, 'MOVING', 2.125114, 101.654321, '2019-07-02 10:00:00'),
+    ('SCOOTER', true, 5, 75, 'MOVING', 2.125114, 101.654321, '2019-07-02 10:00:00'),
     ('SCOOTER', false, 0, 50, 'IDLING', 4.125114, 102.987654, '2019-07-02 10:00:00'),
     ('SCOOTER', true, 0, 15, 'TOWING', 5.125114, 103.456789, '2019-07-02 10:00:00'),
     ('CAR', true, 0, 0, 'TOWING', 5.125114, 103.456789, '2019-07-02 10:00:00');
